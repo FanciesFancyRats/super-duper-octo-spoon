e8c509bfbe2b1b3638e6e3e9eff0ef8315c3cadc
FanciesFancyRats <magriffi1986@gmail>
2017-10-23 17:17:44 -0500

    tried to get some progress on lunch

--- fiddle.py
+++ fiddle.py
@@ -1,4 +1,20 @@
-print("Hello. ") ,
-print("Just another test") ,
-print("")
-print("This is a new line") ,
+def printX():
+ print("----") ,
+def printY():
+ print("|") ,
+def printSpace():
+ print("    ") ,
+def printPlus():
+ print("+") ,
+def newLine():
+ print()
+def printSquare(x, y):
+ countX = 0
+ countY = 0
+ lineX = 0
+ lineY = 0
+ while((countX < x) or (countY < y)): 
+  lineX = x
+  lineY = y
+
+printSquare(1, 1)
