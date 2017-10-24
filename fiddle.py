5867222b4bbe814787a84ad96c2518f5f905bfc9
fanciesfanyrats <magriffi1986@gmail.com>
2017-10-23 19:01:00 -0500

    fiddle not working, not supposed to use while or for

--- fiddle.py
+++ fiddle.py
@@ -1,20 +1,20 @@
-def printX():
- print("----") ,
-def printY():
- print("|") ,
-def printSpace():
- print("    ") ,
-def printPlus():
- print("+") ,
-def newLine():
- print()
-def printSquare(x, y):
- countX = 0
- countY = 0
- lineX = 0
- lineY = 0
- while((countX < x) or (countY < y)): 
-  lineX = x
-  lineY = y
-
-printSquare(1, 1)
+def printX ():
+    print "+ - - - - +" , "- - - - +" , "- - - - +" , "- - - - +"
+def printY(y):
+    print "|         |" , "        |" * y
+def doFour(f, y):
+    f(y)
+    f(y)
+    f(y)
+    f(y)
+def printSquare():
+    printX()
+    doFour(printY(), 3)
+    printX()
+    doFour(printY(), 3)
+    printX()
+    doFour(printY(), 3)
+    printX()
+    doFour(printY(), 3)
+    printX()
+printSquare()
