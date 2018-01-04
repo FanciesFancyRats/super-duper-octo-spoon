3735fd29a7c46854b82f10c4b27552e6cc553ac9
FanciesFancyRats <magriffi1986@gmail.com>
2018-01-04 14:26:55 -0600

    it gets and answer now at least  #

--- ps1-3-2.py
+++ ps1-3-2.py
@@ -13,21 +13,21 @@ air = .2
 
 lowerBound = round(balance/12.0, 2)
 upperBound = float(round((balance * (1 + (air/12))**12)/12, 2))
-bisection = (lowerBound + upperBound) / 2
+bisection = float(round((lowerBound + upperBound)/2, 2)) 
 x = 0
 
-while abs(bisection) > 0.2 and x < 99:
+while x < 99:
     testLower = paymentCycle(lowerBound, balance, air)
     testUpper = paymentCycle(upperBound, balance, air)
     testBisection = paymentCycle(bisection, balance, air)
 
-    print testLower, lowerBound
-    print testUpper, upperBound 
-    print testBisection, bisection 
+    print 'upperbound returns: ', testUpper,'payment: ', upperBound 
+    print 'bisection returns: ', testBisection,'payment: ', bisection 
+    print 'lowerbound returns: ', testLower,'payment: ', lowerBound
     y = raw_input('press a key')
     if abs(testBisection) < 0.2:
         break
-    if abs(testLower - testBisection) < abs(testUpper - testBisection):
+    if abs(testLower) - abs(testBisection) < abs(testUpper) - abs(testBisection):
         upperBound = bisection
         bisection = (lowerBound + upperBound) / 2
     else:
