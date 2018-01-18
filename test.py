c48ace26e8d3b80afb46e2a915d3ad3ef07ff75c
FanciesFancyRats <magriffi1986@gmail.com>
2018-01-18 05:53:25 -0600

    This is just a test

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-CSI="\x1b["
-print CSI+"31;40M" + "Colored Text" + CSI + "0m"
-from termcolor import colored
-
-print colored('hello', 'red'), colored('world', 'green')
+print "this is just another test."
+for i in range (9):
+    print i
